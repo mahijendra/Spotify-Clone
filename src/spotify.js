883c53ed23f14abd5d31953b62069ba3e67b6c79
BVK Mahijendra <bvkmahijendra2@gmail.com>
2021-03-08 20:19:51 +0530

    Final spotify

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "daf6e2c610c9432b81a28317a9a3636d";
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-clone-b82be.web.app";
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
